0d50af3d259236777710614e0ac9d90a1c48f08a
Bucket1572 <sout0811@naver.com>
2021-08-14 16:33:10 +0900

    Model: CNN, RNN 도입, 현재는 LSTM

--- word-embedding/hate_speech_detector_functional.py
+++ word-embedding/hate_speech_detector_functional.py
@@ -1,12 +1,14 @@
 import os
 
-import keras.backend
-
 from data_preprocessor import *
 from tensorflow.keras.layers import Input, Embedding, GlobalAveragePooling1D, Dense
 from tensorflow.keras.layers import BatchNormalization, Dropout
+from tensorflow.keras.layers import GRU, LSTM, SimpleRNN
+from tensorflow.keras.layers import Conv1D, Activation
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
-from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.models import Model
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.utils import plot_model
@@ -29,14 +31,24 @@ if gpus:
 
 # Settings
 # 기본 모델 세팅
+lr = 1e-5
 batch_size = 32
-epochs = 60
-model_name = "stemming-vectorization-dropout-deeper-model"
+epochs = 100
+model_name = "stemming-lstm-bn-model"
 
 # 단어 벡터화를 위한 세팅
 vocab_size = 10000
 sequence_length = 100
 embedding_dim = 16
+
+# CNN 세팅
+num_filters = 64
+kernel_size = 3
+
+# RNN 세팅
+gru_units = 256
+lstm_units = 256
+simple_rnn_units = 128
 
 # 드롭아웃 세팅
 dropout_prob = 0.7
@@ -60,8 +72,17 @@ x_train = parsing_data(x_train, kiwi)
 # y_train 원핫 벡터 변환
 y_train = to_one_hot(y_train)
 
+# 데이터 증식
+x_train, y_train = data_augmentation(
+    x_data=x_train,
+    y_data=y_train,
+    size=5,
+    double_prob=0.3
+)
+
 # Debug
 # print(f"Raw Data:\n{data}\nInputs:\n{x_train}\nLabel:\n{y_train}\n")
+print(len(x_train), len(y_train))
 
 # Test Data
 # Raw 데이터 읽기
@@ -89,6 +110,7 @@ vectorize = TextVectorization(
     output_sequence_length=sequence_length
 )
 vectorize.adapt(x_train)
+vectorize.adapt(x_test)
 
 # 입력 레이어
 inputs = Input(shape=(1,), dtype=tf.string, name="text inputs")
@@ -101,14 +123,44 @@ y = Embedding(
     output_dim=embedding_dim
 )(y)
 
-# Sequential Model -> Functional API
-y = GlobalAveragePooling1D()(y)
-y = Dropout(dropout_prob)(y)
-y = Dense(
-    16,
-    activation='relu'
+# # CNN
+# y = Conv1D(
+#     filters=num_filters,
+#     kernel_size=kernel_size,
+#     strides=1,
+#     padding='same',
+#     kernel_initializer='he_normal',
+#     kernel_regularizer=l2(1e-4)
+# )(y)
+# y = BatchNormalization()(y)
+# y = Activation('relu')(y)
+#
+# y = Conv1D(
+#     filters=num_filters//2,
+#     kernel_size=kernel_size,
+#     strides=1,
+#     padding='same',
+#     kernel_initializer='he_normal',
+#     kernel_regularizer=l2(1e-4)
+# )(y)
+# y = BatchNormalization()(y)
+# y = Activation('relu')(y)
+#
+# RNN
+# y = GRU(
+#     units=gru_units,
+#     return_sequences=True
+# )(y)
+#
+# y = SimpleRNN(units=simple_rnn_units, name="rnn")(y)
+y = LSTM(
+    units=lstm_units,
+    kernel_regularizer=l2(1e-4)
 )(y)
-y = Dropout(dropout_prob)(y)
+
+# DNN
+# y = GlobalAveragePooling1D(name="pooling")(y)
+y = BatchNormalization()(y)
 outputs = Dense(
     3,
     activation='softmax'
@@ -118,22 +170,22 @@ model = Model(inputs=inputs, outputs=outputs)
 
 # 모델 요약
 model.summary()
-plot_model(model, to_file=f"{model_name}.png", show_shapes=True)
+plot_model(model, to_file=f"model-structure/{model_name}.png", show_shapes=True)
 
 model.compile(
-    optimizer='adam',
+    optimizer=RMSprop(learning_rate=lr),
     loss=CategoricalCrossentropy(),
     metrics=['accuracy']
 )
 
 # 체크포인트를 위한 디렉토리 생성
-save_dir = os.path.join(os.getcwd(), 'saved_models')
+save_dir = os.path.join(os.getcwd(), 'saved-models')
 model_name = f'{model_name}.{epochs:03d}.tf'
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 filepath = os.path.join(save_dir, model_name)
 
-# FIXME: 이후 Functional API로 교체 후, Custom callback 만들 것; 현재 modelcheckpoint가 textvectorization과 호환 안 됨.
+# FIXME: Custom callback 만들 것; 현재 modelcheckpoint가 textvectorization과 호환 안 됨.
 # 모델 체크포인트
 checkpoint = ModelCheckpoint(
     filepath=filepath,
@@ -154,4 +206,4 @@ model.fit(
     validation_data=(x_test, y_test),
 )
 
-# model.save_weights(f"saved_models/{model_name}", save_format='tf')
+model.save_weights(f"saved-models/{model_name}", save_format='tf')
