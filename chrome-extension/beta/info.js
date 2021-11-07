f8f4009fa02a8dba705c38dad4fc0dcca9210ad1
Bigbr4in0614 <bigbr4in0614@gmail.com>
2021-11-08 02:51:26 +0900

    20211108 02:51 development in progress

---
+++ chrome-extension/beta/info.js
@@ -0,0 +1,1 @@
+const url = "https://gkkhnyeypg.execute-api.ap-northeast-2.amazonaws.com/rocketll/hate-speech"
--- chrome-extension/beta/js/content.js
+++ chrome-extension/beta/js/content.js
@@ -1,10 +1,20 @@
+import url from "../info"
+
 const LABEL_HATE = "hate"
 const LABEL_OFFENSIVE = "offensive"
 const LABEL_NONE = "none"
+const BATCH_SIZE = 16
 
 function isParagraph(node) {
     if (!node.hasChildNodes()) return false
     if (node.childElementCount == 0) return true
+
+    let innerText = node.innerText.trim()
+    const meaningLessFilterRegex = /\S\D/
+    if (!meaningLessFilterRegex.test(innerText)) return false
+
+    const oneWordFilterRegex = /\s/
+    if (!oneWordFilterRegex.test(innerText)) return false
 
     var children = node.childNodes
     for (let i = 0; i < children.length; i++) {
@@ -18,12 +28,12 @@ class HateBlocker {
     constructor(querySelectorString, level) {
         const allPossibleNodes = document.querySelectorAll(querySelectorString)
         var textNodes = new Array()
-        var texts = ""
+        var texts = new Array()
         
         for (let i = 0; i < allPossibleNodes.length; i++) {
             if (isParagraph(allPossibleNodes[i])) {
                 textNodes.push(allPossibleNodes[i])
-                texts += allPossibleNodes[i].innerText
+                texts.push(allPossibleNodes[i].innerText.trim())
             }
         }
 
@@ -47,24 +57,38 @@ class HateBlocker {
         const longSentences = trimmedSentences.filter((trimmedSentence) => /\s/.test(trimmedSentence))
         this.text = longSentences
     }
+
+    generateBatch() {
+        this.batch = new Array()
+        for (let i = 0; i < this.text.length; i += BATCH_SIZE) {
+            this.batch.push(this.text.slice(i, i + BATCH_SIZE))
+        }
+    }
     
-    generateJson() {
+    generateJson(batch) {
         var jsonObj = new Object()
-        jsonObj.inputs = this.text
+        jsonObj.inputs = batch
         return JSON.stringify(jsonObj)
     }
 
-    static async apiResponse(bodyString) {
-        const url = "https://gkkhnyeypg.execute-api.ap-northeast-2.amazonaws.com/production/hate-speech"
-        const response = await fetch(url, {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: bodyString
-        })
-        return await response.json()
+    async apiResponse() {
+        for (const batch of this.batch) {
+            console.log(batch.length)
+            const response = await fetch(url, {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: this.generateJson(batch)
+            })
+            await response.json().then(
+                (res) => {
+                    console.log("BATCH - BLOCKED")
+                    this.blockHate(res)
+                }
+            )
+        }
     }
 
     blockHate(response) {
@@ -86,9 +110,8 @@ class HateBlocker {
 }
 
 console.log("된다고 된다니까!")
+console.log(document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, div, caption, span, a'))
 const blocker = new HateBlocker('h1, h2, h3, h4, h5, p, li, td, div, caption, span, a', 1)
-blocker.parseText()
-HateBlocker.apiResponse(blocker.generateJson()).then((res) => {
-    console.log("Response was loaded")
-    blocker.blockHate(res)
-})
+// blocker.parseText()
+blocker.generateBatch()
+blocker.apiResponse()
